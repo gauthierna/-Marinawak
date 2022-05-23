b4fb353b3e48ed00d303aab0390d0c7069767fd6
Gauthier Nadaud <gnadaud@gmail.com>
2022-05-23 21:54:43 +0200

    controller booking + form new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+  before_action :booking_params, only: %i[create update]
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render show_path(@boat), status: :unprocessable_entity
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :boat_id)
+  end
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,8 +4,22 @@ class Booking < ApplicationRecord
 
   after_create :set_price
 
+  def create
+  end
+
+  def update
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :boat_id)
+  end
+
   def set_price
     self.amount = (self.end_date - self.start_date) * self.boat.price
     self.save
   end
+
+
 end

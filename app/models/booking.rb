0634015ae267ac4f4dd5b01fdd0ad74e870e8b0b
Gauthier Nadaud <gnadaud@gmail.com>
2022-05-23 18:06:41 +0200

    en cours

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :boat
+
+  after_create :set_price
+
+  def set_price
+    self.amount = (self.end_date - self.start_date) * self.boat.price
+    self.save
+  end
 end

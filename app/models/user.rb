946377c050cfaa60e74682a64242fa085da30e89
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-05-23 16:36:01 +0200

    on peut créer des bateaux

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+  has_many :boats
   validates :first_name, :last_name, presence: true
+
 end

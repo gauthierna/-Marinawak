fd7544c6b9205c41c6843c2940f5011417fa616a
Gauthier Nadaud <gnadaud@gmail.com>
2022-05-24 22:00:21 +0200

    denied + accepted done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,4 @@
 class BookingsController < ApplicationController
-  before_action :booking_params, only: %i[create update accepted]
 
   def create
     @booking = Booking.new(booking_params)
@@ -18,19 +17,23 @@ class BookingsController < ApplicationController
   end
 
   def accepted
-    @booking.status = "accepted"
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.update(status: "accepted")
     redirect_to dashboard_path
   end
 
   def denied
+    @booking = Booking.find(params[:id])
     @booking.status = "denied"
+    authorize @booking
     redirect_to dashboard_path
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date, :status)
   end
 
   def set_boat

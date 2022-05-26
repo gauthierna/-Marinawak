81069af0d468fd90a8fae023a986516ea6485902
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-05-26 22:23:13 +0200

    PgSearch

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,13 @@
 class Boat < ApplicationRecord
+  include PgSearch::Model
+  multisearchable against: [:name, :category]
+
+  pg_search_scope :search_by_columns,
+  against: [ :name, :category ],
+  using: {
+    tsearch: { prefix: true }
+  }
+
   belongs_to :user
   has_many :bookings
   validates :name, :category, :capacity, :price_per_day, presence: true
@@ -6,4 +15,5 @@ class Boat < ApplicationRecord
   validates_associated :user
   validates :capacity, :price_per_day, numericality: { only_integer: true }
   has_many_attached :photos
+
 end
